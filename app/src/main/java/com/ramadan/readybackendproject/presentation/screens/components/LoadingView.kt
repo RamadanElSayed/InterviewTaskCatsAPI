6cf6a9e01c5100c64548208c1e5a0774371d0c98
ramadansayed <ramadan123sayed@gmail.com>
2025-05-09 01:42:49 +0300

    applied enhancements

--- app/src/main/java/com/ramadan/readybackendproject/presentation/screens/components/LoadingView.kt
+++ app/src/main/java/com/ramadan/readybackendproject/presentation/screens/components/LoadingView.kt
@@ -1,6 +1,5 @@
 package com.ramadan.readybackendproject.presentation.screens.components
 
-
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,7 +10,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
